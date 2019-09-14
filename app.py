7f151d3a5e5ce532ee8a3cca9417be66abb752c6
swami84 <44759539+swami84@users.noreply.github.com>
2019-09-14 04:28:17 -0400

    init

---
+++ app.py
@@ -0,0 +1,92 @@
+from flask import Flask, render_template, request, redirect
+import pandas as pd
+import numpy as np
+from datetime import date
+import datetime
+from bokeh.io import output_notebook
+from bokeh.layouts import gridplot
+from bokeh.plotting import figure, show,output_file,save
+from alpha_vantage.timeseries import TimeSeries
+from bokeh.resources import CDN
+from bokeh.embed import file_html
+
+
+
+def datetime(x):
+    return np.array(x, dtype=np.datetime64)
+
+
+app = Flask(__name__)
+app.vars = {}
+
+
+
+  
+
+@app.route('/homepage', methods = ['POST', 'GET'])
+def homepage():
+  
+  if request.method == 'GET':
+    return render_template('homepage.html')
+    
+  else:
+    app.vars['ticker'] = request.form['ticker']
+    return redirect('/display')
+    
+#@app.route('/display', methods = ['GET'])
+##def generateoutput(ticker):
+##    ALPHA_VANTAGE_API_KEY = 'IU2OUWVANSPF10UV'
+##    ts = TimeSeries(key=ALPHA_VANTAGE_API_KEY, output_format='pandas')
+##    df, data_info = ts.get_monthly(ticker)
+##    data = df.copy()
+##    data = data.reset_index()
+##    data['Avg Price'] = (data['2. high'] + data ['3. low'])/2
+##
+##  
+##    p1 = figure(x_axis_type="datetime", title=ticker +  " Stock Price")
+##    p1.grid.grid_line_alpha=0.6
+##    p1.xaxis.axis_label = 'Date'
+##    p1.yaxis.axis_label = 'Price'
+##    p1.line((datetime(data['date'])), data['Avg Price'], color='#1729CB',line_width = 4, legend='Avg Price')
+##    p1.circle((datetime(data['date'])), data['1. open'], size=6, legend='Opening Price',color='green', alpha=0.5)
+##    p1.circle((datetime(data['date'])), data['4. close'], size=6, legend='Closing Price',color='red', alpha=0.5)
+##    p1.legend.location = "top_left"
+##    
+##
+##    output_file("\templates\stocks.html", title="Stock Chart")
+##    #return render_template('display.html')
+##    return redirect('/display')
+@app.route('/display', methods = ['GET', 'POST'])
+def  display():
+    ALPHA_VANTAGE_API_KEY = 'IU2OUWVANSPF10UV'
+    ts = TimeSeries(key=ALPHA_VANTAGE_API_KEY, output_format='pandas')
+    df, data_info = ts.get_monthly(app.vars['ticker'])
+    data = df.copy()
+    data = data.reset_index()
+    data['Avg Price'] = (data['2. high'] + data ['3. low'])/2
+
+  
+    p1 = figure(x_axis_type="datetime", title =  app.vars['ticker'] +  " Stock Price")
+    p1.grid.grid_line_alpha=0.6
+    p1.xaxis.axis_label = 'Date'
+    p1.yaxis.axis_label = 'Price'
+    p1.line((datetime(data['date'])), data['Avg Price'], color='#1729CB',line_width = 4, legend='Avg Price')
+    p1.circle((datetime(data['date'])), data['1. open'], size=6, legend='Opening Price',color='green', alpha=0.5)
+    p1.circle((datetime(data['date'])), data['4. close'], size=6, legend='Closing Price',color='red', alpha=0.5)
+    p1.legend.location = "top_left"
+    
+
+    output_file("templates/display.html", title="Stock Chart")
+    #from shutil import copyfile
+    #copyfile("stock.html", "templates/stock.html")
+    show(p1)
+    #save(p1, filename = "\templates\stocks1.html")
+    
+    
+    
+    return render_template('display.html')
+    
+
+if __name__ == '__main__':
+  app.run(debug= True)
+
